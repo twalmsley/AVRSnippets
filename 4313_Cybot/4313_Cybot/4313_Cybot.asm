8fdd7a421e2e48897e64bb45235e0768b227a4fa
Tony Walmsley <tonywalmsley@gmail.com>
2013-05-27 18:52:20 +0100

    Slight improvements - collision detection and simple recovery.

--- 4313_Cybot/4313_Cybot/4313_Cybot.asm
+++ 4313_Cybot/4313_Cybot/4313_Cybot.asm
@@ -31,6 +31,10 @@
 ;    C O N S T A N T S   T O   C H A N G E
 ; ============================================
 ;
+.EQU	FORWARD = 0x0A
+.EQU	BACKWARD = 0x05
+.EQU	LEFT = 0x09
+.EQU	RIGHT = 0x06
 ;
 ; ============================================
 ;  F I X + D E R I V E D   C O N S T A N T S
@@ -47,15 +51,66 @@
 .DEF	gpReg = r16
 .DEF	mainLoop = r17
 .def	switches = r18
+.def	direction = r19
 .DEF	tmr1 = r24
 .DEF	tmr2 = r25
 .DEF	tmr3 = r23
 ;
 ; ============================================
+;     MACROS
+; ============================================
+;
+.MACRO	INC_DRIVE_COUNT
+		lds gpReg, DRIVE_COUNT_LOC_LO
+		inc gpReg
+		sts DRIVE_COUNT_LOC_LO, gpReg
+		brne skip_overflow
+		lds gpReg, DRIVE_COUNT_LOC_HI
+		inc gpReg
+		sts DRIVE_COUNT_LOC_HI, gpReg
+skip_overflow:
+.ENDMACRO
+
+.MACRO	RESET_DRIVE_COUNT
+		ldi gpReg, 0x00
+		sts DRIVE_COUNT_LOC_LO, gpReg
+		sts DRIVE_COUNT_LOC_HI, gpReg
+.ENDMACRO
+
+.MACRO	CHECK_DRIVE_COUNT
+		lds gpReg, DRIVE_COUNT_LOC_HI
+		cpi gpReg, 0x60
+.ENDMACRO
+
+.MACRO ENABLE_PCI
+		ldi gpReg, 1<<3		;i.e. PCIE1
+		out GIMSK, gpReg
+		ldi gpReg, 1<<PCINT9 | 1<<PCINT8
+		out PCMSK1, gpReg
+		sei					; enable interrupts
+.ENDMACRO
+
+.MACRO INIT_STACK
+		cli
+		ldi r16, low(RAMEND)
+		ldi r17, high(RAMEND)
+		out SPL, R16
+		out SPH, R17
+.ENDMACRO
+; ============================================
 ;       S R A M   D E F I N I T I O N S
 ; ============================================
 ;
 .DSEG
+CUR_DIR_LOC:
+			.DB 0x00
+DRIVE_COUNT_LOC_LO:
+			.DB 0x00
+DRIVE_COUNT_LOC_HI:
+			.DB 0x00
+RECOVER_DIR_LOC:
+			.DB 0x00
+
 ; Format: Label: .BYTE N ; reserve N Bytes from Label:
 ;
 ; ============================================
@@ -82,14 +137,7 @@
 ; ============================================
 ;
 RESET:
-;
-; Set the stack pointer
-;
-		cli
-		ldi r16, low(RAMEND)
-		ldi r17, high(RAMEND)
-		out SPL, R16
-		out SPH, R17
+		INIT_STACK
 ;
 ; Set the IO Ports
 ;
@@ -101,50 +149,54 @@ RESET:
 		cbi PORTB, 4
 
 		ldi gpReg, 0x03
-		out DDRA, gpReg		;Port A bits 0 and 1 are for the bumber switches
+		out DDRA, gpReg		;Port A bits 0 and 1 are for the bumper switches
+
+		ENABLE_PCI
+; Set the direction to forward and store it in SRAM
 ;
-; Initialise everything
+		ldi gpReg, FORWARD
+		sts CUR_DIR_LOC, gpReg
 ;
-		rcall init_ext_interrupt
-		sei					; enable interrupts
+; Set the recover direction to LEFT and store in SRAM
+;
+		ldi gpReg, LEFT
+		sts RECOVER_DIR_LOC, gpReg
 ;
 ; ============================================
 ;         P R O G R A M    L O O P
 ; ============================================
+;
 loop:
-		rcall forward
+;
+; Start in the default direction
+;
+		lds direction, CUR_DIR_LOC
+		rcall startmoving
+;
+; Increment the drive count
+;
+		INC_DRIVE_COUNT
+		CHECK_DRIVE_COUNT
+		brne loop
+;
+; After a number of iterations reset the count and switch the recovery direction
+;
+		RESET_DRIVE_COUNT
+
         rjmp loop
 ;
 ; ============================================
 ;         Subroutines
 ; ============================================
-stop:
+stopmoving:
 		in gpReg, PINB
 		andi gpReg, 0xF0
 		out PORTB, gpReg
 		ret
-back:
+startmoving:
 		in gpReg, PINB
 		andi gpReg, 0xF0
-		ori gpReg, 0x05
-		out PORTB, gpReg
-		ret
-left:
-		in gpReg, PINB
-		andi gpReg, 0xF0
-		ori gpReg, 0x09
-		out PORTB, gpReg
-		ret
-right:
-		in gpReg, PINB
-		andi gpReg, 0xF0
-		ori gpReg, 0x06
-		out PORTB, gpReg
-		ret
-forward:
-		in gpReg, PINB
-		andi gpReg, 0xF0
-		ori gpReg, 0x0a
+		or gpReg, direction
 		out PORTB, gpReg
 		ret
 ;---------------------------------------------
@@ -162,26 +214,20 @@ delay_msLoop:
 		dec tmr3
 		brne delay_next
 		ret
-init_ext_interrupt:
-;
-; Enable PCI for port A1 and A0
-;
-		ldi gpReg, 1<<3		;i.e. PCIE1
-		out GIMSK, gpReg
-		ldi gpReg, 1<<PCINT9 | 1<<PCINT8
-		out PCMSK1, gpReg
-		ret
+;---------------------------------------------
 ;---------------------------------------------
 EXTInt0:
 		cli
-		rcall back
+recover:
+		ldi direction, BACKWARD
+		rcall startmoving
 		ldi gpReg, 0x60
 		rcall delay_ms
-		rcall stop
-		rcall left
-		ldi gpReg, 0x60
+		lds direction, RECOVER_DIR_LOC
+		rcall startmoving
+		ldi gpReg, 0x30
 		rcall delay_ms
-		rcall stop
+		rcall stopmoving
 int_done:
 		sei
 		reti
