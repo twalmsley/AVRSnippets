0468131c48c650339f0e81122799e1f375d726dd
Tony Walmsley <tonywalmsley@gmail.com>
2013-07-07 16:53:01 +0100

    Mainly LCD driver code in C.

---
+++ LCD_ATMega328/LCD_ATMega328/LCD_ATMega328.cpp
@@ -0,0 +1,16 @@
+/*
+ * LCD_ATMega328.cpp
+ *
+ * Created: 04/07/2013 21:28:15
+ *  Author: tony
+ */
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code
+    }
+}
