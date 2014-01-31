56ba2c3bb47575b76306eb8422461e64c5a55b18
Tony Walmsley <tonywalmsley@gmail.com>
2014-01-31 20:05:13 +0000

    ATMega328 code to send commands to a rover bot over the serial port

---
+++ 328_bot/328_bot.c
@@ -0,0 +1,43 @@
+/*
+ * _4313_bot.c
+ *
+ * Created: 03/01/2014 16:34:53
+ *  Author: tony
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdlib.h>
+
+
+void USART_Transmit(unsigned char data) {
+	while(!(UCSR0A & (1<<UDRE0)));
+	UDR0 = data;
+}
+
+unsigned char USART_Receive() {
+	while(!(UCSR0A & (1<<RXC0)));
+
+	return UDR0;
+}
+
+int main(void) {
+	//
+	// Init the USART
+	//
+	UBRR0H = 0;
+	UBRR0L = 25;//19.2Kbaud
+	UCSR0A = _BV(U2X0);
+	UCSR0B = _BV(TXEN0);
+	UCSR0C = _BV(UCSZ00) | _BV(UCSZ01);
+	while(1) {
+		USART_Transmit('R');
+		_delay_ms(4000);
+		USART_Transmit('F');
+		USART_Transmit(0x10);
+		USART_Transmit(0x00);
+		_delay_ms(15000);
+		USART_Transmit('L');
+		_delay_ms(4000);
+	}
+}
